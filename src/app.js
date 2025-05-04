dbf244cd1e746cb390d0d98dfc9fe9b732b5588e
Jonathan0318 <jonathan0318@icloud.com>
2025-05-04 23:45:45 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let pronouns = ['the', 'our'];
+  let adjectives = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let extensions = ['.com', '.net', '.io'];
+
+  let domainList = document.getElementById('domainList');
+
+  for (let p of pronouns) {
+    for (let a of adjectives) {
+      for (let n of nouns) {
+        for (let ext of extensions) {
+          let fullDomain = p + a + n + ext;
+          addToList(fullDomain);
+        }
+      }
+    }
+  }
+
+  function addToList(domain) {
+    const li = document.createElement('li');
+    li.textContent = domain;
+    domainList.appendChild(li);
+  }
+
+  console.log(]);
 };
